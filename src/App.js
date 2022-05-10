0eb3e7b9250c2050a73dfd56de5cc2bd457101e4
gasparg <gaspi-gonzalez@hotmail.com>
2022-05-10 20:07:55 -0300

    Creando app react

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Micro3D Productos & Diseños
         </a>
       </header>
     </div>
